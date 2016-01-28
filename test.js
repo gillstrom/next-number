cba80407e330dd75bc842130117f2c6bc7622b73
Andreas Gillström <andreasgillstrom@gmail.com>
2016-01-28 17:32:42 +0100

    Adjust tests according to new AVA api

--- test.js
+++ test.js
@@ -1,22 +1,20 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test(t => {
-	const next = fn(50, a => a + 1);
+	const next = m(50, a => a + 1);
 
 	t.is(next(), 51);
 	t.is(next(), 52);
 	t.is(next(), 53);
 	t.is(next(), 54);
-	t.end();
 });
 
 test(t => {
-	const next = fn(1, a => (a / 2) * 3);
+	const next = m(1, a => (a / 2) * 3);
 
 	t.is(next(), 1.5);
 	t.is(next(), 2.25);
 	t.is(next(), 3.375);
 	t.is(next(), 5.0625);
-	t.end();
 });
